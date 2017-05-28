6640cbf32cc10f977f76a5e940d56de2e7bdf4a8
Jormangandur <=>
2017-05-28 16:03:30 +0100

    Initial commit, basic models + route outline

---
+++ models.py
@@ -0,0 +1,85 @@
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship, sessionmaker
+from sqlalchemy import create_engine
+from passlib.apps import custom_app_context as pwd_context
+import random
+import string
+from itsdangerous import(
+    TimedJSONWebSignatureSerializer as Serializer, BadSignature, SignatureExpired)
+
+Base = declarative_base()
+
+
+class User(Base):
+    __tablename__ = 'user'
+    id = Column(Integer, primary_key=True)
+    username = Column(String(32), index=True)
+    picture = Column(String)
+    email = Column(String, index=true)
+    password_hash = Column(String(64))
+
+    def hash_password(self, password):
+        self.password_hash = pwd_context.encrypt(password)
+
+    def verify_password(self, password):
+        return pwd_context.verify(password, self.password_hash)
+
+    def generate_auth_token(self, expiration=600):
+        s = Serializer(secret_key, expires_in=expiration)
+        return s.dumps({'id': self.id})
+
+    # Add a method to verify auth tokens here
+    @staticmethod
+    def verify_auth_token(token):
+        s = Serializer(secret_key)
+        try:
+            data = s.loads(token)
+        except SignatureExpired:
+            # Valid, but expired token
+            return None
+        except BadSignature:
+            # Invalid token
+            return None
+        user_id = data['id']
+        return user_id
+
+
+class Request(Base):
+    __tablename__ = 'base'
+    id = Column(Integer, primary_key=True)
+    meal_type = Column(String)
+    location_string = Column(String)
+    latitude = Column(string)
+    longitude = Column(String)
+    meal_time = Column(String)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
+
+
+class Proposal(Base):
+    __tablename__ = 'proposal'
+    id = Column(Integer, primary_key=True)
+    to_user = Column(Integer, ForeignKey('user.id'))
+    from_user = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
+    request_id = Column(Integer, ForeignKey('request.id'))
+    request = relationship(Request)
+
+
+class MealDate(Base):
+    __tablename__ = 'meal_date'
+    id = Column(Integer, primary_key=True)
+    user_1 = Column(Integer, ForeignKey('user.id'))
+    user_2 = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
+    restaurant_name = Column(String)
+    restaurant_address = Column(String)
+    restaurant_picture = Column(String)
+    meal_time = Column(String)
+
+
+engine = create_engine('sqlite:///meet-n-eat.db')
+
+
+Base.metadata.create_all(engine)
---
+++ views.py
@@ -0,0 +1,119 @@
+from models import Base, User, Request, Proposal, MealDate
+from flask import Flask, jsonify, request, url_for, abort, g
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship, sessionmaker
+from sqlalchemy import create_engine
+from functools import update_wrapper
+import time
+import json
+from flask_httpauth import HTTPBasicAuth
+from oauth2client.client import flow_from_clientsecrets
+from oauth2client.client import FlowExchangeError
+import httplib2
+from flask import make_response
+import requests
+from redis import Redis
+
+redis = Redis()
+auth = HTTPBasicAuth()
+engine = create_engine('sqlite:///paleKale.db')
+Base.metadata.bind = engine
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
+app = Flask(__name__)
+
+
+@app.route('/api/v1/<str:provider>/login', methods=["POST"]):
+def login(provider):
+    pass
+
+
+@app.route('/api/v1/<str:provider>/logout', methods=["POST"]):
+def logout(provider):
+    pass
+
+
+@app.route('/api/v1/users', methods=["GET"]):
+def get_users():
+    pass
+
+
+@app.route('/api/v1/users', methods=["POST"]):
+def make_user():
+    pass
+
+
+@app.route('/api/v1/users', methods=["PUT", 'DELETE']):
+def update_delete_user():
+    pass
+
+
+@app.route('/api/v1/users/<int:id>', methods=["GET"]):
+def get_user(id):
+    pass
+
+
+@app.route('/api/v1/requests', methods=["GET"]):
+def get_requests():
+    pass
+
+
+@app.route('/api/v1/requests', methods=["POST"]):
+def make_request():
+    pass
+
+
+@app.route('/api/v1/requests/<int:id>', methods=["GET"]):
+def get_request(id):
+    pass
+
+
+@app.route('/api/v1/requests/<int:id>', methods=["PUT", "DELETE"]):
+def update_request(id):
+    pass
+
+
+@app.route('/api/v1/proposals', methods=["GET"]):
+def get_proposals():
+    pass
+
+
+@app.route('/api/v1/proposals', methods=["POST"]):
+def make_proposal():
+    pass
+
+
+@app.route('/api/v1/proposals/<int:id>', methods=["GET"]):
+def get_proposal(id):
+    pass
+
+
+@app.route('/api/v1/proposals/<int:id>', methods=["PUT", "DELETE"]):
+def update_proposal(id):
+    pass
+
+
+@app.route('/api/v1/dates', methods=["GET"]):
+def get_dates():
+    pass
+
+
+@app.route('/api/v1/dates', methods=["POST"]):
+def make_date():
+    pass
+
+
+@app.route('/api/v1/dates/<int:id>', methods=["GET"]):
+def get_date(id):
+    pass
+
+
+@app.route('/api/v1/dates/<int:id>', methods=["PUT", "DELETE"]):
+def udpate_date(id):
+    pass
+
+
+if __name__ == '__main__':
+    app.secret_key = 'secret_key_very_secret'
+    app.debug = True
+    app.run(host='0.0.0.0', port=5000)
